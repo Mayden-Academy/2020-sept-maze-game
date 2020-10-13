e9882a316832c7cdc3cb3b563444d894f5c4dac9
Ellie <erobinson98@btinternet.com>
2020-10-13 13:58:46 +0100

    timer built

---
+++ script.js
@@ -0,0 +1,26 @@
+function startTimer( duration, element) {
+    timerRunning = true;
+    let timeLeft = duration;
+    let timer = setInterval( () => {
+        let minutes = parseInt(timeLeft / 60, 10);
+        let seconds = parseInt(timeLeft % 60, 10);
+
+        minutes = (minutes < 10) ? ("0" + minutes) : minutes;
+        seconds = (seconds < 10) ? ("0" + seconds) : seconds;
+
+        element.textContent = minutes + ":" + seconds;
+
+        if (--timeLeft < 0) {
+            timeLeft = duration;
+            clearInterval(timer)
+            console.log('end of game')
+        }
+    }, 1000)
+}
+let timerRunning = false;
+document.querySelector('#button').addEventListener('mouseout', ()=> {
+    if ( timerRunning === false) {
+        startTimer(10, document.querySelector('#timer p'));
+    }
+})
+
