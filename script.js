1bc95358e848637da3f5edf20530d57b153be701
Josh Field <josh.t.field@bigpond.com>
2020-10-13 13:27:08 +0100

    changed cursor on start button, changed splash-main to id

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //event listener on the start button to make splashscreen disappear
 document.querySelector('.start-button').addEventListener('click', () => {
 
-    document.querySelector('.splash-main').style.display = 'none';
+    document.querySelector('#splash-main').style.display = 'none';
     document.querySelector('#gameScreen').style.display = 'block';
 
 });
