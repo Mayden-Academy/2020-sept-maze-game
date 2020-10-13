b8e4075dcc99f5371ab53055f29273d3ffa1ae6f
Jenny Allton-Twist <jennyalltontwist@gmail.com>
2020-10-13 10:02:25 +0100

    preliminary pop up skeleton and logic to open win popup

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+// Get the modal
+let modal = document.querySelector("#gameWinPopup")
+
+// Get the button that opens the modal if you've won
+let Tempbtn = document.querySelector(".start-button")
+
+// Get the button that closes the modal and restarts the game
+let restartButton = document.querySelector(".restartButton")
+
+// When the user clicks the start button, open the modal (this needs changing to when the game ends, open modal)
+//eventlistener this!!
+Tempbtn.onclick = function() {
+    modal.style.display = "block"
+}
+
+
+// When the user clicks on (restart button), close the modal
+//eventlistener this!!
+restartButton.onclick = function() {
+    modal.style.display = "none"
+    //startGame Function here
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none"
+    }
+}
+
