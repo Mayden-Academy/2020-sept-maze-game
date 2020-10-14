cecfac0752b619b08a2f9c2e460e031c322ff609
Josh Field <josh.t.field@bigpond.com>
2020-10-14 10:03:47 +0100

    made grimreaper popout

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ function listenForCollisions() {
         item.addEventListener('mouseenter', (e) => {
             // console.log('collision mouseOver: Oh dear, you lost as you left the path or collided with a monster!');
             alert('Oh dear, you lost as you left the path or collided with a monster!');
+        })
+        item.addEventListener('mouseover', (e) => {
+            // console.log('collision mouseOver: Oh dear, you lost as you left the path or collided with a monster!');
+            alert('Oh dear, you lost as you left the path or collided with a monster!');
             e.stopPropagation();
         })
     })
@@ -19,7 +23,7 @@ function listenForCollisions() {
 
 //win if u get to Exit door (no timers yet)
 function listenForWinning() {
-    document.querySelector('.winningSquare').addEventListener('mouseover', (e) => {
+    document.querySelector('.winningSquare').addEventListener('mouseenter', (e) => {
         // console.log('winningSquare mouseover: Congrats! You made it to the Exit!');
         alert('Congrats! You made it to the Exit!');
         e.stopPropagation();
@@ -28,9 +32,10 @@ function listenForWinning() {
 
 //call functions from here
 
-let ghost = document.querySelector('.ghostImage');
+let ghost = document.querySelector('.ghost');
 let pumpkin = document.querySelector('.pumpkinImage');
 let reaper = document.querySelector('.grim_reaperImage');
+let lineSeven = document.querySelector('.line_seven');
 
 let start = document.querySelector('.start_area');
 
@@ -53,22 +58,41 @@ function startAnimation() {
     });
     pumpkin.animate([
         {transform: 'translateX(0px)'},
-        {transform: 'translateX(-120px)'},
-        {transform: 'translate(0px)'},
-        {transform: 'translate(120px)'},
-        {transform: 'translate(0px)'}
+        {transform: 'translateX(120px)'},
+        {transform: 'translateX(0px)'},
+        {transform: 'translateX(-140px)'},
+        {transform: 'translateX(0px)'}
     ], {
         // timing options
-        duration: 3000,
+        duration: 6000,
         iterations: Infinity
     });
-    reaper.animate([
-        {transform: 'translateY(0px)'},
-        {transform: 'translateY(100px)'},
-        {transform: 'translateY(0px)'}
-    ], {
-        // timing options
-        duration: 3000,
-        iterations: Infinity
-    });
+    lineSeven.addEventListener('mouseleave', (e) => {
+        reaper.animate([
+            {transform: 'translateY(0px)'},
+            {transform: 'translateY(100px)'},
+            {transform: 'translateY(0px)'}
+        ], {
+            // timing options
+            duration: 3000,
+            iterations: Infinity
+        });
+    })
 }
+
+function homeScreenGhost () {
+    document.querySelector('.start-button').addEventListener('mouseover', (e) => {
+        e.stopPropagation()
+        document.querySelector('.game-logo').animate([
+            {transform: 'translateY(0px)'},
+            {transform: 'translateY(-60px)'},
+            {transform: 'translateY(0px)'}
+        ], {
+            // timing options
+            duration: 3000,
+            iterations: Infinity
+        });
+    })
+}
+
+homeScreenGhost()
