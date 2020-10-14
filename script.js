3d671624e4705dc0287445b077be9dbf3c62ab28
Josh Field <josh.t.field@bigpond.com>
2020-10-14 12:45:57 +0100

    working reset

--- script.js
+++ script.js
@@ -1,139 +1,56 @@
-
-// Get the modal
-let winModal = document.querySelector("#gameWinPopup")
-let loseModal = document.querySelector("#gameLosePopup")
-
-// code here to activate win modal displaying
-    let win = function() {
-        winModal.style.display = "block"
-    }
-
-// code here to activate lose modal displaying
-
-let lose = function() {
-   loseModal.style.display = "block"
-}
-
-//close the modal because there is no restart button
-document.querySelector('#closeLoseModal').addEventListener('click', () =>{
-    loseModal.style.display = "none"
-})
-
-document.querySelector('#closeWinModal').addEventListener('click', () =>{
-    winModal.style.display = "none"
-})
-
-//event listener on the start button to make splashscreen disappear
-document.querySelector('.start-button').addEventListener('click', () => {
-    document.querySelector('#splash-main').style.display = 'none';
-    document.querySelector('#gameScreen').style.display = 'block';
-});
-
-
-//listen for collisions with obstacles
-function listenForCollisions() {
-    document.querySelectorAll('.collision').forEach(item => {
-        item.addEventListener('mouseenter', (e) => {
-            lose()
-            e.stopPropagation();
-        })
-    })
-    document.querySelector('.ghost').
-}
-
-//win if u get to Exit door (no timers yet)
-function listenForWinning() {
-    document.querySelector('.winningSquare').addEventListener('mouseenter', (e) => {
-        win()
-        e.stopPropagation();
-    })
-}
-
-//call functions from here
-
 let ghost = document.querySelector('.ghost');
 let pumpkin = document.querySelector('.pumpkinImage');
 let reaper = document.querySelector('.grim_reaperImage');
 let lineSeven = document.querySelector('.line_seven');
 
 let start = document.querySelector('.start_area');
-
-start.addEventListener('mouseleave', (e) => {
-    e.stopPropagation()
-    startAnimation();
-    listenForCollisions();
-    listenForWinning();
-})
+let collision = document.querySelectorAll('.collision');
 
 
-function startAnimation() {
-    ghost.animate([
-        {transform: 'translateY(0px)'},
-        {transform: 'translateY(150px)'},
-        {transform: 'translateY(0px)'}
-    ], {
-        // timing options
-        duration: 3000,
-        iterations: Infinity
-    });
-    pumpkin.animate([
-        {transform: 'translateX(0px)'},
-        {transform: 'translateX(120px)'},
-        {transform: 'translateX(0px)'},
-        {transform: 'translateX(-140px)'},
-        {transform: 'translateX(0px)'}
-    ], {
-        // timing options
-        duration: 6000,
-        iterations: Infinity
-    });
-    console.log(ghost);
-    lineSeven.addEventListener('mouseleave', (e) => {
-        reaper.animate([
-            {transform: 'translateY(0px)'},
-            {transform: 'translateY(100px)'},
-            {transform: 'translateY(0px)'}
-        ], {
-            // timing options
-            duration: 3000,
-            iterations: Infinity
-        });
-    })
-}
-//     ghost.animate([
-//         {transform: 'translateY(0px)'},
-//         {transform: 'translateY(150px)'},
-//         {transform: 'translateY(0px)'}
-//     ], {
-//         // timing options
-//         duration: 3000,
-//         iterations: Infinity
-//     });
-//     pumpkin.animate([
-//         {transform: 'translateX(0px)'},
-//         {transform: 'translateX(120px)'},
-//         {transform: 'translateX(0px)'},
-//         {transform: 'translateX(-140px)'},
-//         {transform: 'translateX(0px)'}
-//     ], {
-//         // timing options
-//         duration: 6000,
-//         iterations: Infinity
-//     });
-//
-//     lineSeven.addEventListener('mouseleave', (e) => {
-//         reaper.animate([
-//             {transform: 'translateY(0px)'},
-//             {transform: 'translateY(100px)'},
-//             {transform: 'translateY(0px)'}
-//         ], {
-//             // timing options
-//             duration: 3000,
-//             iterations: Infinity
-//         });
-//     })
-// }
+// Get the modal
+let winModal = document.querySelector("#gameWinPopup")
+let loseModal = document.querySelector("#gameLosePopup")
 
+// code to animate the obstacles
+let ghostMove = ghost.animate([
+    {transform: 'translateY(0px)'},
+    {transform: 'translateY(150px)'},
+    {transform: 'translateY(0px)'}
+], {
+    // timing options
+    duration: 3000,
+    iterations: Infinity
+});
+ghostMove.pause()
+
+let pumpkinMove = pumpkin.animate([
+    {transform: 'translateX(0px)'},
+    {transform: 'translateX(120px)'},
+    {transform: 'translateX(0px)'},
+    {transform: 'translateX(-140px)'},
+    {transform: 'translateX(0px)'}
+], {
+    // timing options
+    duration: 6000,
+    iterations: Infinity
+});
+pumpkinMove.pause()
+
+/*lineSeven.addEventListener('mouseleave', (e) => {*/
+let reaperMove = reaper.animate([
+    {transform: 'translateY(0px)'},
+    {transform: 'translateY(100px)'},
+    {transform: 'translateY(0px)'}
+], {
+    // timing options
+    duration: 3000,
+    iterations: Infinity
+});
+reaperMove.pause()
+
+// })
+
+// code to make splash screen mouse hover
 function homeScreenGhost () {
     document.querySelector('.start-button').addEventListener('mouseover', (e) => {
         e.stopPropagation()
@@ -150,5 +67,73 @@ function homeScreenGhost () {
 }
 
 homeScreenGhost()
+
+// code here to activate win modal displaying
+function win() {
+    start.removeEventListener('mouseleave', runGame);
+    collision.forEach(() => {
+        this.removeEventListener('mouseenter', lose)
+    })
+    winModal.style.display = "block";
+    ghostMove.pause()
+    pumpkinMove.pause()
+    reaperMove.pause()
+}
+
+// code here to activate lose modal displaying
+
+function lose() {
+    start.removeEventListener('mouseleave', runGame);
+    collision.forEach(() => {
+        this.removeEventListener('mouseenter', lose)
+    })
+    loseModal.style.display = "block";
+    ghostMove.pause()
+    pumpkinMove.pause()
+    reaperMove.pause()
+}
+
+//close the modal because there is no restart button
+document.querySelectorAll('.endButton').forEach((button) =>{
+    button.addEventListener('click', (e) =>{
+        e.stopPropagation();
+        start.addEventListener('mouseleave', runGame);
+        loseModal.style.display = "none";
+        winModal.style.display = "none";
+    })
+})
+
+//event listener on the start button to make splashscreen disappear
+document.querySelector('.start-button').addEventListener('click', () => {
+    document.querySelector('#splash-main').style.display = 'none';
+    document.querySelector('#gameScreen').style.display = 'block';
+    start.addEventListener('mouseleave', runGame)
+});
+
+
+//listen for collisions with obstacles
+function listenForCollisions() {
+    collision.forEach(item => {
+        item.addEventListener('mouseenter', lose);
+    })
+}
+
+//win if u get to Exit door (no timers yet)
+function listenForWinning() {
+    document.querySelector('.winningSquare').addEventListener('mouseenter', (e) => {
+        win()
+        e.stopPropagation();
+    })
+}
+
+//call functions from here
+
+function runGame() {
+    ghostMove.play()
+    pumpkinMove.play()
+    reaperMove.play()
+    listenForCollisions();
+    listenForWinning();
+}
 
 
